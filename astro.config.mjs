bb383d694239fd2a8dc4bf9349f3827302905615
Kent <kent@podi.dk>
2024-07-04 14:50:01 +0200

    Removed sqoosh

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig, squooshImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 
-export default defineConfig({
-  image: {
-    service: squooshImageService(),
-  },
-});
+// https://astro.build/config
+export default defineConfig({});
